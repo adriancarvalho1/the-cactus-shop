4d15341bac05cee3f445272261f4d43c1d5e2f06
adriancarvalho1 <61248591+adriancarvalho1@users.noreply.github.com>
2020-10-28 18:18:32 +0000

    Add files via upload

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 const Navbar = () => (
     <nav>
       <ul class="nav-links">
-        <li><a href="./index">Home</a></li>
-        <li><a href="./about">About</a></li>
-        <li><a href="./contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
 
